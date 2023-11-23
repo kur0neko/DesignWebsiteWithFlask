45ecfab2e4094109d34e7c797f7ea628198456b1
Noah Nguyen <noahn1105@MSI.localdomain>
2023-11-22 20:32:32 -0800

    merge conflicts

--- Web App Tester/app/routes.py
+++ Web App Tester/app/routes.py
@@ -2,9 +2,8 @@ from flask import render_template
 from flask import redirect
 from flask import flash
 from flask import session
-from flask import request
-from .forms import CreateAccountForm, LoginForm, Notebox, NewNoteButton, EditNoteButton, Editbox
-from app.models import User, Note
+from .forms import CreateAccountForm, LoginForm, Notebox, TableParams, updateName, updatePassword, NewNoteButton, EditNoteButton, Editbox
+from app.models import User, Note, Table
 from app import myapp_obj
 from app import db
 
@@ -14,18 +13,19 @@ def index():
 
 @myapp_obj.route("/home")
 def home():
-		return render_template('base.html')
+		return render_template('home.html')
 
 @myapp_obj.route("/login", methods=['GET', 'POST'])
 def login():
 	form = LoginForm()
 	if form.validate_on_submit():
-		found_user = User.query.filter_by(username=form.username.data).first()
+		found_user = User.query.filter_by(username=form.username.data, password=form.password.data).first()
 		if found_user:
 			session['user'] = form.username.data
 			print('you logged in! it worked!')
+			return redirect('/home')
 		else:
-			redirect('/createaccount')
+			return redirect('/createaccount')
 	return render_template('login.html', form=form)
 
 @myapp_obj.route('/logout')
@@ -49,27 +49,40 @@ def createaccount():
 
     return render_template('createaccount.html', form=form)
 
-@myapp_obj.route("/profile", methods=['POST', 'GET'])
+@myapp_obj.route("/profile", methods=['GET', 'POST'])
 def profile():
 	if 'user' in session:
 		newnote = NewNoteButton()
 		editbutton = EditNoteButton()
 		user = session['user']
-
 		found_user = User.query.filter_by(username=session['user']).first()
 		if found_user:
 			session['id'] = found_user.id
 
 		note_list = {}
-		num = 0
 		found_id = Note.query.filter_by(user_id = session['id']).all()
 		if found_id:
 			for note in found_id:
-				note_list[note.note_name] = (note.note_body, editbutton)
+				note_list[f'{note.note_name}'] = (note.note_body, editbutton)
 
+		found_user = User.query.filter_by(id = session['id']).first()
+		changeName = updateName()
+		changeName.username = found_user.username
+		if changeName.validate_on_submit():
+			found_user.username = changeName.newname.data
+			session['user'] = found_user.username
+			db.session.commit()
+			return redirect('/profile')
+
+		changePassword = updatePassword()
+		changePassword.password = found_user.password
+		if changePassword.validate_on_submit():
+			found_user.password = changeName.newpassword.data
+			db.session.commit()
+			return redirect('/profile')
 	else:
 		return redirect('/login')
-	return render_template('profile.html', user=user, note_list=note_list,newnote=newnote)
+	return render_template('profile.html', user=user, note_list=note_list, changeName = changeName, changePassword= changePassword, newnote=newnote)
 
 @myapp_obj.route('/newnote', methods=['GET', 'POST'])
 def newnote():
@@ -84,7 +97,33 @@ def newnote():
 			return redirect('/profile')
 	else:
 		return redirect('/login')
-	return render_template('newnote.html', note=note)
+	return render_template('newnote.html', note= note)
+
+@myapp_obj.route('/newtable', methods=['GET', 'POST'])
+def newtable():
+	if 'user' in session:
+
+		user = session['user']
+		found_user = User.query.filter_by(username=session['user']).first()
+		if found_user:
+			session['id'] = found_user.id
+			
+		table = TableParams()
+		table_list = {}
+		found_id = Table.query.filter_by(user_id = session['id']).all()
+		if found_id:
+			for dbtable in found_id:
+				table_list[f'{dbtable.table_name}'] = (dbtable.numRows, dbtable.numColumns)
+		
+		if table.validate_on_submit():
+			print('table creation')
+			u = Table(table_name= table.name.data,  numRows = table.rows.data, numColumns = table.columns.data, user_id = session['id'])
+			db.session.add(u)
+			db.session.commit()
+		
+	else:
+		return redirect('/login')
+	return render_template('newtable.html', table = table, table_list = table_list)
 
 @myapp_obj.route('/editnote/<notename>', methods=['GET', 'POST'])
 def editnote(notename):
@@ -98,8 +137,6 @@ def editnote(notename):
 			print()
 			db.session.commit()
 			return redirect('/profile')
-		
 	return render_template('editnote.html', editnote=editnote)
-
 
 
